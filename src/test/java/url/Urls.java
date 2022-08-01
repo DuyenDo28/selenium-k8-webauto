cd5eafce95b7b2e4f965df3222f2ee184c2a2e80
Do <duyen.do@datalogic.com>
2022-08-01 16:20:08 +0700

    submit lesson20

--- src/test/java/url/Urls.java
+++ src/test/java/url/Urls.java
@@ -2,10 +2,14 @@ package url;
 
 public interface Urls {
 
+    String demoBaseUrl = "http://demowebshop.tricentis.com";
     String baseUrl = "https://the-internet.herokuapp.com";
+    String loginSlug = "/login";
     String dropdownSlug = "/dropdown";
     String iframeSlug = "/iframe";
     String hoverSlug = "/hovers";
     String jsAlertSlug = "/javascript_alerts";
+    String dynamicControlSlug = "/dynamic_controls";
+    String floatingMenuSlug = "/floating_menu";
 
 }
