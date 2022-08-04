f443fdc2a81398565871753e97ebfeb57f5c9106
Do <duyen.do@datalogic.com>
2022-08-04 09:53:31 +0700

    lesson21, Updated getComSelector method

--- src/test/java/models/components/Component.java
+++ src/test/java/models/components/Component.java
@@ -76,7 +76,7 @@ public class Component {
             return By.cssSelector(componentClass.getAnnotation(ComponentCssSelector.class).value());
         }
         else if(componentClass.isAnnotationPresent(ComponentXpathSelector.class)){
-            return By.cssSelector(componentClass.getAnnotation(ComponentXpathSelector.class).value());
+            return By.xpath(componentClass.getAnnotation(ComponentXpathSelector.class).value());
         }else{
             throw new IllegalArgumentException("Component class" + componentClass + "must have annotation"
                     + ComponentCssSelector.class.getSimpleName() + "or" + ComponentXpathSelector.class.getSimpleName());
