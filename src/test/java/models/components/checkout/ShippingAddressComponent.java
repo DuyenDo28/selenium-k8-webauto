1a97ee63429d646a7b8bc04852909ec0b30a0b7a
Do <duyen.do@datalogic.com>
2022-09-19 14:14:36 +0700

    Add lesson27 and lesson28

--- src/test/java/models/components/checkout/ShippingAddressComponent.java
+++ src/test/java/models/components/checkout/ShippingAddressComponent.java
@@ -2,13 +2,23 @@ package models.components.checkout;
 
 import models.components.Component;
 import models.components.ComponentCssSelector;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-@ComponentCssSelector("#opc-billing")
-public class BillingAddressComponent extends Component {
+@ComponentCssSelector("#opc-shipping")
+public class ShippingAddressComponent extends Component {
 
-    public BillingAddressComponent(WebDriver driver, WebElement component) {
+    private static final By continueBtnSel = By.cssSelector(".new-address-next-step-button");
+
+    public ShippingAddressComponent(WebDriver driver, WebElement component) {
         super(driver, component);
+    }
+
+    public void clickOnContinueBtn(){
+        WebElement continueButton = component.findElement(continueBtnSel);
+        continueButton.click();
+        wait.until(ExpectedConditions.invisibilityOf(continueButton));
     }
 }
--- src/test/java/models/pages/CheckoutOptionsPage.java
+++ src/test/java/models/pages/CheckoutOptionsPage.java
@@ -1,2 +1,18 @@
-package models.pages;public class CheckOutOptionsPage {
+package models.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutOptionsPage extends BasePage {
+
+    private static final By checkoutAsGuestBtn = By.cssSelector(".checkout-as-guest-button");
+
+    public CheckoutOptionsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void checkoutAsGuest(){
+        component.findElement(checkoutAsGuestBtn).click();
+    }
+
 }
